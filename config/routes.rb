f838aba862b386cfc9c622aed884b15fce418fb8
ayazahmadtarar <ayazahmad@ymail.com>
2018-01-15 22:59:47 +0500

    changes made

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   get 'welcome/index'
+  get 'welcome/welcome_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
